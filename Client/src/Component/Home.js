8a592e1fb8b2109130fb614e461123dbba0232d7
OuadieZerhouni <Ouadie.Zerhouni@usmba.ac.ma>
2023-01-09 17:13:36 +0100

    .

--- Client/src/Component/Home.js
+++ Client/src/Component/Home.js
@@ -1,18 +1,13 @@
-//component wit three button with className="form-button" 
+//component wit three button with className="form-button"
 import React from "react";
 
 export default function Home() {
-    return (
-        <div>
-        <button className="form-button"><a href="/Add/Departement">Insert Departement</a>
-        </button>
-        <button className="form-button"><a href="/Add/Filiere">Insert Filiere</a>
-        </button>
-        <button className="form-button"><a href="/Add/Reunion">Insert Reunion</a>
-        </button>
-        <button className="form-button"> <a href="/Add/Professeur" >Insert Professeur</a>
-        </button>
-
-        </div>
-    );
-    }
+  return (
+    <div>
+      <a href="/Add/Departement">Insert Departement</a>
+      <a href="/Add/Filiere">Insert Filiere</a>
+      <a href="/Add/Reunion">Insert Reunion</a>
+      <a href="/Add/Professeur">Insert Professeur</a>
+    </div>
+  );
+}
