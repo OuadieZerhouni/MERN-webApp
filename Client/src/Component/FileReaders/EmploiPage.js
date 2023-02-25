ff0215c84e600073b635e5bd58e129c352345301
OuadieZerhouni <Ouadie.Zerhouni@usmba.ac.ma>
2023-02-25 21:42:32 +0100

    .

--- Client/src/Component/FileReaders/EmploiPage.js
+++ Client/src/Component/FileReaders/EmploiPage.js
@@ -1,51 +1,45 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
-import { Document, Page,  pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import '../ComponentCSS/PDFReader.css';
 
 
 function EmploiReader() {
-  const [pdfUrl, setPdfUrl] = useState(null);
   const [numPages, setNumPages] = useState(null);
+  const [images, setImages] = useState([]);
   const API_DATABASE = process.env.REACT_APP_API_DATABASE;
+  const[Download, setDownload] = useState('');
 
   useEffect(() => {
-    axios
-      .post(
-        API_DATABASE + "/option/Emploi_temps",
-        { _id: window.location.pathname.split("/")[2] },
-        {
-          headers: {
-            Authorization: "Bearer " + localStorage.getItem("token"),
-          },
-          responseType: "arraybuffer",
-        }
-      )
-      .then((response) => {
-        const blob = new Blob([response.data], { type: "application/pdf" });
-        const url = URL.createObjectURL(blob);
-        setPdfUrl(url);
+    axios.post(API_DATABASE+'/option/Emploi_temps', { _id: window.location.pathname.split("/")[2]},
+    {headers: {Authorization: "Bearer " + localStorage.getItem("token")}})
+      .then(response => {
+        setDownload(response.data.path.replace("/images",".pdf"))
+        let imageDirPath=response.data.path
+        setNumPages(response.data.numPages);
+        for (let i = 1; i <= numPages; i++) {
+          setImages(images => [...images, `${imageDirPath}${"/page-"+i}.png`]);
+        }   
+
+
+
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
-      });
-  }, [API_DATABASE]);
+      }
+    );
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+  }, [API_DATABASE, numPages]);
+return (
+  <div className="PDF-container">
+    <a href={Download}  className="Download-Link">Download</a>
+    {images.map((image, index) => (
+      <img className="Page-pdf" key={index} src={image} alt="Emploi du temps" />
+    ))}
+  </div>
+);
 
-  return (
-    <div>
-      {pdfUrl && (
-        <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
-          {Array.from(new Array(numPages), (el, index) => (
-            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-          ))}
-        </Document>
-      )}
-    </div>
-  );
 }
+
 
 export default EmploiReader;
