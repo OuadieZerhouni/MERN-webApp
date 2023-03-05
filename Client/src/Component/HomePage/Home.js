076c00fd9f9cf3bc20bee8132cf6df8b4c12e73f
OuadieZerhouni <Ouadie.Zerhouni@usmba.ac.ma>
2023-03-05 13:37:45 +0100

    scroll effecr (ouadie)

--- Client/src/Component/HomePage/Home.js
+++ Client/src/Component/HomePage/Home.js
@@ -1,29 +1,33 @@
 import React, { useEffect } from "react";
+import { useState , useRef} from "react";
 import Post from "../HomePage/Post";
 import "../ComponentCSS/Home.css";
 import Header from "../Header";
 
 
 export default function Home() {
+  const logoRef = useRef(null);
+  const [opacity, setOpacity] = useState(1);
  
-
+    function handleScroll() {
+      const scrollPosition = window.scrollY;
+      const opacity = 1 - Math.min(scrollPosition / 200, 1);
+      console.log(opacity);
+      setOpacity(opacity);
+    }
+  
   return (
-    <div className="bg-cont">
+    <div className="bg-cont" onScroll={handleScroll}>
       <Header />
-      <div className="logo-center">
-
+      <div className="logo-center" ref={logoRef} >
+        {/* content of the logo-center element */}
       </div>
-      <div className="Home-cont">
-        <div className="first-div">
-        
-
-        </div>
-        <div className="second-div">
+      <div className="Home-cont" >
+        <div className="first-div"></div>
+        <div className="second-div"  >
           <Post />
-
         </div>
-        </div>
-        
+      </div>
     </div>
   );
 }
