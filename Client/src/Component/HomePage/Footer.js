2e1749cddc454d3bbac1ffcb91fb2aff878e47da
FUJITSU <FUJITSU@DESKTOP-IV0DDR2>
2023-04-07 00:06:02 +0000

    Last touches footer

--- Client/src/Component/HomePage/Footer.js
+++ Client/src/Component/HomePage/Footer.js
@@ -1,63 +1,124 @@
-import React from 'react';
-import "../../CSS/ComponentCSS/Footer.css"
-
+import React from "react";
+import "../../CSS/ComponentCSS/Footer.css";
 
 export default function Footer() {
   return (
     <footer>
       <div id="footer-div-1">
         <div id="social-media">
-          
-          
-          
-          
-          <a href=""><i class="fa-brands fa-square-facebook"></i></a>
-          <a href=""><i class="fa-brands fa-instagram"></i></a>
-          <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
-          <a href=""><i class="fa-brands fa-twitter"></i></a>
-          <a href="https://www.google.fr/maps/place/33%C2%B059'22.6%22N+5%C2%B000'06.9%22W/@33.989603,-5.001903,14z/data=!4m4!3m3!8m2!3d33.989603!4d-5.001903?hl=fr"><i class="fa-solid fa-location-dot"></i></a>
+          <a target={"_blank"} href="https://www.facebook.com/usmba.ac.ma/" id="facebook">
+            <i class="fa-brands fa-square-facebook fa-2xl"></i>
+          </a>
+          <a target={"_blank"} href="https://www.instagram.com/usmba_fes/" id="instagram">
+            <i class="fa-brands fa-instagram fa-2xl"></i>
+          </a>
+          <a target={"_blank"} href="https://www.linkedin.com/company/usmba/" id="linkedin">
+            <i class="fa-brands fa-linkedin-in fa-2xl"></i>
+          </a>
+          <a target={"_blank"} href="https://twitter.com/usmbacom" id="twitter">
+            <i class="fa-brands fa-twitter fa-2xl"></i>
+          </a>
+          <a
+            target={"_blank"}
+            href="https://www.google.fr/maps/place/33%C2%B059'22.6%22N+5%C2%B000'06.9%22W/@33.989603,-5.001903,14z/data=!4m4!3m3!8m2!3d33.989603!4d-5.001903?hl=fr"
+            id="location"
+          >
+            <i class="fa-solid fa-location-dot fa-2xl"></i>
+          </a>
         </div>
-          
-        <div id="links">
-          <ul>
-            <li class="footer-ul-title">Est services</li>
-            <li><a href="http://elearn.est-usmba.ac.ma/est/">Moodle</a></li>
-            <li><a href="http://services.est-usmba.ac.ma/etudiant/login/">E-Notes</a></li>
-            <li><a href="http://www.est-usmba.ac.ma/CONCOURSESTfes.php">Concours</a></li>
-            <li><a href=" http://www.est-usmba.ac.ma/duESTFES.php">Suivi des lauréats</a></li>
-          </ul>
-          <ul>
-          <li class="footer-ul-title">Departements</li>
-            <li><a href="">G.Informatique</a></li>
-            <li><a href="">Technique de managment</a></li>
-            <li><a href="">G.Mecanique</a></li>
-            <li><a href="">G.Procedes</a></li>
-          </ul>
-          <ul>
-          <li class="footer-ul-title">Autre liens</li>
-            <li><a href="http://www.est-usmba.ac.ma/Cours.php">Cours</a></li>
-            <li><a href="http://www.est-usmba.ac.ma/contact.php">Contact</a></li>
-            <li><a href=" http://www.est-usmba.ac.ma/rech_activ.php">Activités de l'EstF</a></li>
-            
-            
-            
-          </ul>
-          
+
+        <div id="footer-links">
+          <a class="logo-container" href="/">
+            USMBA
+          </a>
+          <div id="links">
+            <ul>
+              <li class="footer-ul-title">Est services</li>
+              <li>
+                <a target={"_blank"} href="http://elearn.est-usmba.ac.ma/est/">
+                  Moodle
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href="http://services.est-usmba.ac.ma/etudiant/login/"
+                >
+                  E-Notes
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href="http://www.est-usmba.ac.ma/CONCOURSESTfes.php"
+                >
+                  Concours
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href=" http://www.est-usmba.ac.ma/duESTFES.php"
+                >
+                  Suivi des lauréats
+                </a>
+              </li>
+            </ul>
+
+            <ul>
+              <li class="footer-ul-title">Autre liens</li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href="http://www.est-usmba.ac.ma/Cours.php"
+                >
+                  Cours
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href="http://www.est-usmba.ac.ma/contact.php"
+                >
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a
+                  target={"_blank"}
+                  href=" http://www.est-usmba.ac.ma/rech_activ.php"
+                >
+                  Activités de l'EstF
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div id="footer-div-2">
-        <h1>USMBA</h1>
-        <div>
-        <ul>
-          <li><a href="">Home</a></li>
-          <li><a href="">Privacy</a></li>
-          <li><a href="">About Us</a></li>
-          <li><a href="">Terms Of Use</a></li>
-          <li><a href="">CopyRight @</a></li>
-        </ul>
-          <h4>©Copyright USMBA 2023. <a href="https://github.com/Abdessamad-Belmadani">Abdessamad Belmadani</a> <a href="https://github.com/OuadieZerhouni">Ouadie Zerhouni</a></h4>
+        <div id="footer-credits">
+          <ul>
+            <li>
+              <a target={"_blank"} href="https://www.termsfeed.com/live/ecfe9b74-24da-4c30-8c5d-479a9edf753d">
+                Terms Of Use
+              </a>
+            </li>
+            <li>
+              <a target={"_blank"} href="">
+                ©Copyright USMBA 2023
+              </a>
+            </li>
+          </ul>
+
+          <h4>
+          <span>Develepée par</span>
+            <a target={"_blank"} href="https://github.com/Abdessamad-Belmadani">
+              Abdessamad Belmadani
+            </a>
+            <span id="dot"> •</span>
+            <a target={"_blank"} href="https://github.com/OuadieZerhouni">
+              Ouadie Zerhouni
+            </a>
+          </h4>
         </div>
-        
       </div>
     </footer>
   );
